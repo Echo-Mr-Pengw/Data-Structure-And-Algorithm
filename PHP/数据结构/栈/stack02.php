94f176f2790c81317fdef83b5d8ba510444977ae
pengw <2943927631@qq.com>
2020-06-22 00:53:59 +0800

    Update stack02.php
    增加注释

--- PHP/数据结构/栈/stack02.php
+++ PHP/数据结构/栈/stack02.php
@@ -6,11 +6,36 @@
 
 class Stack {
 
-	public $stackSpace;
-	public $stackElement;
+	/**
+	 * [$top1 指向栈1的栈顶(数组的首部)]
+	 * @var [type]
+	 */
 	public $top1;
+
+	/**
+	 * [$top2 指向栈2的栈顶(数组的尾部)]
+	 * @var [type]
+	 */
 	public $top2;
 
+	/**
+	 * [$stackSpace 栈空间大小]
+	 * @var [type]
+	 */
+	public $stackSpace;
+
+	/**
+	 * [$stackElement 栈中元素]
+	 * @var [type]
+	 */
+	public $stackElement;
+
+	/**
+	 * [__construct 初始化]
+	 * @Author   pengw
+	 * @DateTime 2020-06-22T00:47:06+0800
+	 * @param    int|integer              $stackSpace [栈大小]
+	 */
 	public function __construct(int $stackSpace = 0) {
 
 		$this->top1 = -1;
@@ -19,7 +44,15 @@ class Stack {
 		$this->stackElement = array_fill(0, $stackSpace, NULL);
 	}
 
-	public function push(string $element, int $stackType = 1) {
+	/**
+	 * [push 入栈操作]
+	 * @Author   pengw
+	 * @DateTime 2020-06-22T00:47:19+0800
+	 * @param    string                   $element   [入栈元素]
+	 * @param    int|integer              $stackType [1表示1号栈 2表示2号栈]
+	 * @return   [string]                            [description]
+	 */
+	public function push(string $element, int $stackType = 1): string {
 
 		if(empty($this->stackSpace)) {
 			return '未分配栈空间';
@@ -49,7 +82,14 @@ class Stack {
 		return '入栈成功';
 	}
 
-	public function pop(int $stackType = 1) {
+	/**
+	 * [pop 出栈操作]
+	 * @Author   pengw
+	 * @DateTime 2020-06-22T00:48:07+0800
+	 * @param    int|integer              $stackType [1表示1号栈 2表示2号栈]
+	 * @return   [string]                            [description]
+	 */
+	public function pop(int $stackType = 1): string {
 
 		if(empty($this->stackSpace)) {
 			return '未分配栈空间';
@@ -84,7 +124,14 @@ class Stack {
 		return $e;
 	}
 
-	public function showStackElement(int $stackType = 0) {
+	/**
+	 * [showStackElement 显示栈中的元素]
+	 * @Author   pengw
+	 * @DateTime 2020-06-22T00:48:53+0800
+	 * @param    int|integer              $stackType [0表示显示两个栈的元素 1表示显示1号栈 2表示显示2号栈]
+	 * @return   [array]                             [description]
+	 */
+	public function showStackElement(int $stackType = 0): array {
 
 		if($stackType == 0) {
 			return $this->stackElement;
@@ -98,16 +145,16 @@ class Stack {
 			return array_slice($this->stackElement, $this->top2);
 		}
 
-		return 'error';
+		return [];
 	}
 }
 
 $s = new Stack(6);
 var_dump($s->push(9, 1));
-var_dump($s->push(9, 1));
-var_dump($s->push(9, 1));
-var_dump($s->push(9, 1));
-var_dump($s->push(9, 1));
-var_dump($s->push(9, 1));
+var_dump($s->push(8, 1));
+var_dump($s->push(7, 1));
+var_dump($s->push(6, 1));
+var_dump($s->push(5, 1));
+var_dump($s->push(4, 1));
+var_dump($s->push(3, 1));
 var_dump($s->showStackElement(0));
-
